5b87c88c2d4294f52bc05c9d52af676dadba86f2
chang <2622310479@qq.com>
2022-11-15 17:08:50 +0800

    feat: Building the Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,62 @@
-import type { Component } from 'solid-js';
+import type { Component } from 'solid-js'
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import WoodenFish from './WoodenFish.svg'
+import { AresChang } from './components/AresChang'
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div
+      w-100vw
+      h-100vh
+      px-14
+      py-6
+      text-center
+      bg='#111'
+      color-white
+      flex='~ col'
+      justify-between
+      select-none>
+      <header>
+        <div text-left>
+          <AresChang />
+        </div>
+        <div text-8xl font-bold>
+          79
+        </div>
+        <div color='#444' text-xl>
+          功德
+        </div>
       </header>
-    </div>
-  );
-};
 
-export default App;
+      <main flex justify-center>
+        <img src={WoodenFish} alt='WoodenFish' />
+      </main>
+
+      <footer color='#444'>
+        <div>
+          按下<code>Space</code>或<code>Click</code>积攒功德
+        </div>
+        <div>
+          点击<code>左上角Logo</code>开启/关闭 沉浸模式
+        </div>
+        <div flex justify-between text-lg>
+          <a
+            href='https://www.github.com/ares-chang/wooden-fish'
+            target='_blank'
+            hover:color='#eee'
+            flex
+            items-center
+            gap-1>
+            <i i-carbon-logo-github inline-block></i>
+            <span>GitHub</span>
+          </a>
+          <a href='https://www.github.com/ares-chang' hover:color='#eee'>
+            @AresChang
+          </a>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default App
