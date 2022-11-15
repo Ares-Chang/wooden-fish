91c78196eecc712ab9ff547a1a28392abe7974f4
chang <2622310479@qq.com>
2022-11-15 16:03:30 +0800

    feat: create AresChang Logo module

---
+++ src/components/AresChang.tsx
@@ -0,0 +1,16 @@
+export function AresChang() {
+  return (
+    <a href='https://areschang.top' target='_blank'>
+      <span
+        text-3xl
+        font-bold
+        color-transparent
+        style={{
+          background: 'linear-gradient( 315deg, #42d392 25%, #647eff )'
+        }}
+        bg-clip-text="!">
+        Ares Chang
+      </span>
+    </a>
+  )
+}
