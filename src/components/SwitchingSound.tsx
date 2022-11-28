5b0c9f1dd5c3e61e26bc97202d3395bbbc729cb1
chang <2622310479@qq.com>
2022-11-28 21:48:41 +0800

    feat: 添加阻止冒泡事件

--- src/components/SwitchingSound.tsx
+++ src/components/SwitchingSound.tsx
@@ -14,7 +14,7 @@ export default function SwitchingSound(prop: { close: () => void }) {
       onClick={prop.close}>
       <div flex-1>
         <div text-3xl>背景音量</div>
-        <div my-20>
+        <div my-20 onClick={e => e.stopPropagation()}>
           <div h-10px bg-white='/70'>
             <div w-100 h-full bg-white flex justify-end items-center>
               <div w-10 h-10 bg-white='/80' rd-5></div>
