87e6a60558760bfe2c08ae212cec38e360bd164c
chang <2622310479@qq.com>
2022-11-29 18:37:23 +0800

    feat: 添加 slider 组件拖动

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,73 @@
+import { onMount, createSignal } from 'solid-js'
+
+let line: HTMLDivElement
+let point: HTMLDivElement
+
+let downX // 点击时位置
+let [moveX, setMoveX] = createSignal(10)
+let [bg, setBg] = createSignal('red')
+/**
+ * 鼠标按下事件
+ * @param e
+ */
+function mouseDown(e: MouseEvent) {
+  setBg('pink')
+  downX = e.clientX
+  console.log(e, downX)
+  // 绑定鼠标移动及松开事件
+  document.addEventListener('mousemove', mouseMove)
+  document.addEventListener('mouseup', mouseUp)
+}
+
+/**
+ * 鼠标移动事件
+ * @param e
+ */
+function mouseMove(e: MouseEvent) {
+  let val = e.clientX
+  if (val <= 0) val = 0
+  else if (val >= line.offsetWidth) val = line.offsetWidth
+
+  setMoveX(val)
+
+  console.log(moveX())
+}
+
+/**
+ * 鼠标松开事件
+ * @param e
+ */
+function mouseUp(e: MouseEvent) {
+  // 解除鼠标移动及松开事件绑定
+  document.removeEventListener('mousemove', mouseMove)
+  document.removeEventListener('mouseup', mouseUp)
+}
+
+export default function Silder() {
+  onMount(() => {
+    console.log(line.offsetWidth, point)
+    console.log(moveX())
+  })
+  return (
+    <div ref={line} h-10px bg-white='/70'>
+      <div
+        h-full
+        bg-white
+        flex
+        justify-end
+        items-center
+        style={{
+          width: moveX() + 'px'
+        }}>
+        <div
+          ref={point}
+          w-10
+          h-10
+          bg-white='/80'
+          rd-5
+          cursor-pointer
+          onMouseDown={mouseDown}></div>
+      </div>
+    </div>
+  )
+}
