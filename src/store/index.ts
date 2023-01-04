849d082e2d503b18b3359e7a04c06c8c10d27b51
chang <2622310479@qq.com>
2023-01-04 11:37:57 +0800

    perf: 动态获取需要缓存的字段名称

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,12 @@ import {
   UseConfigOptions
 } from '../composables/useConfig'
 
-// 本地缓存字段名称
-const localList = ['volume']
+const configData = useGetConfig() // 本地缓存数据
+const localList = Object.keys(configData) // 本地缓存字段名称
+
+/**
+ * 不需要缓存的字段类型
+ */
 interface StoreProps {
   [key: string]: any
 }
@@ -14,7 +18,7 @@ interface StoreProps {
  * 获取缓存并初始化 store 存储
  */
 export const [store, setStore] = createStore<UseConfigOptions & StoreProps>({
-  ...useGetConfig()
+  ...configData
 })
 
 /**
